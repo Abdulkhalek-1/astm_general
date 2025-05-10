6caa2a4055653c1bf2989b7f03b57da2e2e6c679
Abdulkhalek-1 <abdulkhalek.muhammad.work@gmail.com>
2025-05-10 16:49:23 +0300

    Update configuration files for local log paths and ports; add .gitignore for log files and cache

--- astm_interactive_conf.py
+++ astm_interactive_conf.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-log_filename='/var/log/astm_interactive.log'
+log_filename='./astm_interactive.log'
 host_address='127.0.0.1'
-host_port='2222'
+host_port='4001'
 select_timeoutt=1
